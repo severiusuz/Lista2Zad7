6f7d81a19a1e306dffb4150e6e73761e04762c0c
severiusuz <severiusuz@gmail.com>
2017-05-19 14:25:18 +0200

    init

---
+++ Zad7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cart = new ShoppingCart();
+            foreach(Product item in cart)
+            {
+                Console.WriteLine(item.Name);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Zad7/ShoppingCart.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Zad7
+{
+    class ShoppingCart : IEnumerable, IEnumerator
+    {
+        private List<Product> cart = new List<Product>();
+        private int position = -1;
+
+        public ShoppingCart()
+        {
+            cart.Add(new Product { Name = "jeden" });
+            cart.Add(new Product { Name = "dwa" });
+            cart.Add(new Product { Name = "trzy" });
+        }
+
+        public object Current => cart.ElementAt(position);
+
+        public IEnumerator GetEnumerator()
+        {
+            return this;
+        }
+
+        public bool MoveNext()
+        {
+            position++;
+            return (position < cart.Count);
+        }
+
+        public void Reset()
+        {
+            position = 0;
+        }
+    }
+}
